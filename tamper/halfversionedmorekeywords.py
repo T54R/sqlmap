874aede9e8da29dcc86e844686aacd9c2f55a865
T54R <32045227+T54R@users.noreply.github.com>
2023-11-18 22:58:33 +0300

    Update halfversionedmorekeywords.py
    special tamper skips much of MODSECURITY & more others

--- tamper/halfversionedmorekeywords.py
+++ tamper/halfversionedmorekeywords.py
@@ -42,7 +42,7 @@ def tamper(payload, **kwargs):
     def process(match):
         word = match.group('word')
         if word.upper() in kb.keywords and word.upper() not in IGNORE_SPACE_AFFECTED_KEYWORDS:
-            return match.group().replace(word, "/*!0%s" % word)
+            return match.group().replace(word, "/*!50000%s*/ " % word)
         else:
             return match.group()
 
@@ -50,6 +50,6 @@ def tamper(payload, **kwargs):
 
     if payload:
         retVal = re.sub(r"(?<=\W)(?P<word>[A-Za-z_]+)(?=\W|\Z)", process, retVal)
-        retVal = retVal.replace(" /*!0", "/*!0")
+        retVal = retVal.replace(" /*!50000", "+/*!50000")
 
     return retVal
